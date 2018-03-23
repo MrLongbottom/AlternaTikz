d92329f5d41f08e0f28c948da90f539e37ac11a3
unknown <nschmi16@student.aau.dk>
2018-03-23 15:52:13 +0100

    libary added

--- AlternaTikz/src/scanner/Scanner.java
+++ AlternaTikz/src/scanner/Scanner.java
@@ -1,10 +1,17 @@
 package scanner;
 
+import org.javatuples.Triplet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scanner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String A = "test", B = "test";
+		int C = 5;
+		Triplet<String, String, Integer> testT = new Triplet<>(A,B,C);
+		List<Triplet<String, String, Integer>> testL = new ArrayList<>();
 	}
-
 }
